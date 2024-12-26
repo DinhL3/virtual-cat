22be779773f5c4092802104801f5d20e86f3a667
Dinh Le <lqdquangdinh@gmail.com>
2024-12-26 20:02:46 +0200

    add: container, color palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'virtual-cat';
+  title = 'Virtual Cat';
 
   showAlert() {
     alert('Button clicked!');
