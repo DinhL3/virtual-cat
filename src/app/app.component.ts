cd9ce70c69f5d9d4bb086636e21572d62fb71de3
Dinh Le <lqdquangdinh@gmail.com>
2024-12-26 16:48:12 +0200

    hello world starter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'virtual-cat';
+
+  showAlert() {
+    alert('Button clicked!');
+  }
 }
